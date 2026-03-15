c7df82eb6826e2114da120853433e53127cfd0e2
kanagalakranthisree2007 <kanagalakranthisree0@gmail.com>
2026-03-15 08:21:14 +0530

    Add files via upload

---
+++ - personal_intro.py
@@ -0,0 +1,8 @@
+# personal_intro.py
+name = input("What is your name? ")
+age = input("How old are you? ")
+hobby = input("What is your favorite hobby? ")
+
+print("\n🎉 Welcome", name, "! 🎉")
+print(f"You are {age} years old and love {hobby}.")
+print("The Developers Arena -- the world where only develoepers exist!")
